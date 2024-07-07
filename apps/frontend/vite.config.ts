967b517e25917a5f4fbbfb3c6b3f66bfc407618f
thirdmadman <thirdmadman@gmail.com>
2024-07-08 00:32:39 +0200

    feat: add VITE_UI_PORT

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode } : { mode: string } ) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: parseInt(process.env.VITE_UI_PORT || '0', 10) || 3000
+    }
+  });
+}
