1c82453dd9da471d1e93838e277b1c929df5a818
thirdmadman <thirdmadman@gmail.com>
2024-07-07 17:17:46 +0200

    feat: add list of names route

--- apps/backend/src/routes/root.ts
+++ apps/backend/src/routes/root.ts
@@ -3,6 +3,7 @@ import {IReply} from '@/types/interfaces/fastify';
 import {findConfigByName} from '@/services/utils/findConfigByName';
 import {runJnlpFile} from '@/services/utils/runJnlpFile';
 import {generateJnlpFile} from '@/services/file-generators/generateJnlpFile';
+import { readConfig } from '@/services/utils/readConfig';
 
 export async function rootRoutes(server: FastifyInstance) {
   server.get('/status', {}, async (request, reply) => {
@@ -11,6 +12,30 @@ export async function rootRoutes(server: FastifyInstance) {
     } catch (error) {
       request.log.error(error);
       return reply.send(500);
+    }
+  });
+  server.get<{Reply: IReply<{names: Array<string>}>}>('/', {}, async (request, reply) => {
+    try {
+      const config = await readConfig();
+
+      if (config === null || undefined) {
+        return reply.code(404).send({error: 'Config file not found'});;
+      }
+
+      const names = config.kvmList.map((kvmConfig) => kvmConfig.name);
+
+      if (!names || names.length <= 0) {
+        return reply.code(404).send({error: 'Config list empty'});
+      } else {
+        return reply.code(200).send({
+          success: true,
+          data: {
+            names
+          }
+        });
+      }
+    } catch (error) {
+      return reply.code(400).send({error: error as string});
     }
   });
   server.get<{Params: {name: string}; Reply: IReply<{}>}>('/:name', {}, async (request, reply) => {
