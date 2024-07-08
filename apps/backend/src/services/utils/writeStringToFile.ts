909f92d31a016ee7490dfb971ca41ee8999f4075
thirdmadman <thirdmadman@gmail.com>
2024-07-08 14:46:41 +0200

    feat: update writeStringToFile

--- apps/backend/src/services/utils/writeStringToFile.ts
+++ apps/backend/src/services/utils/writeStringToFile.ts
@@ -1,12 +1,23 @@
 import fs from 'fs';
 
-export async function writeStringToFile(filePath: string, content: string) {
-  const writeStream = fs.createWriteStream(filePath, { flags: 'w' });
-  return new Promise((resolve, reject) => {
-    writeStream.write(content, 'utf8', () => {
-      writeStream.end();
+export async function writeStringToFile(filePath: string, content: string): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const writeStream = fs.createWriteStream(filePath, { flags: 'w' });
+
+    writeStream.on('error', (err) => {
+      reject(err);
     });
-    writeStream.on('finish', () => resolve(true));
-    writeStream.on('error', reject);
+
+    writeStream.on('finish', () => {
+      resolve();
+    });
+
+    writeStream.write(content, 'utf8', (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        writeStream.end();
+      }
+    });
   });
-} 
+}
